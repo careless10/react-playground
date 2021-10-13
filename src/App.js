a60e941efca24b3c45efea91f90bae552134f5ec
Menawer <m--0@hotmail.com>
2021-10-13 18:10:39 +0300

    simple items list

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import React,{useState} from 'react'
 
 
 function App() {
-  let dishes = [ 'Sour Dough', 'Baryani', 'Pizza', 'Ribs', 'Salmon' ];
+  const [data, setData] = useState([])
 
-  return <div>
-    {dishes.map(x => <p>{x}</p>)}
+  return <div className="App">
+    <div className="container">
+      <h3>My Items</h3>
+      <input />
+      <button>Add Item!</button>
+      <div className="items-list">
+        <p>Latte</p>
+        <p>Mocha</p>
+      </div>
+    </div>
   </div>;
 }
 
