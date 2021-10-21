b84832c9202b344299d0c37adfd6060dcc55afce
Menawer <m--0@hotmail.com>
2021-10-21 18:47:21 +0300

    bootstrap installed

--- src/App.js
+++ src/App.js
@@ -1,17 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
 import React,{useState,useEffect} from 'react'
+import { Button } from 'reactstrap';
 
 
 function App() {
-  const [total, setTotal] = useState(0)
+  const [total, setTotal] = useState(0);
+  const [data, setData] = useState({})
 
-  console.log("App rendered")
+  useEffect(newCat, [])
+
+  console.log("outside useeffect")
+  
+  useEffect(() => {
+    console.log("Inside useeffect")
+  }, [])
+
+  async function newCat(){
+    let raw = await fetch('https://thatcopy.pw/catapi/rest/');
+    let res = await raw.json();
+    setData(res);
+  }
+
+
   return <div>
     <h1>Total {total}</h1>
-    <Voter total={total} setTotal={setTotal} title="للي يستحق" />
-    <Voter total={total} setTotal={setTotal} title="مشرف" />
-    <Voter total={total} setTotal={setTotal} title="ولد عمي" />
+    <img src={data.url} width="300"/>
+    <Button color="danger">Danger!</Button>
     </div>
 }
 
@@ -23,7 +38,7 @@ function Voter(props){
     setVotes(votes+1)
   }
 
-  console.log(props.title+" rendered");
+  // console.log(props.title+" rendered");
   return <div>
     <button onClick={voted}>{props.title}</button>
     <h3>Votes: {votes}</h3>
